f13f11bb70a9a251df13f9d232d67b9d09be17c4
uaidri <uaidri@gmail.com>
2021-01-19 12:58:43 -0300

    DepartmentDao Implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -7,6 +7,11 @@ public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+		
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 
 }
